dfebfcb707d92b309e99c9af6eab73e030a0d395
balaji-18-05 <balaji9750452687@gmail.com>
2026-01-30 22:23:23 +0530

    tasks update

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,49 @@ import React from "react";
 import "./App.css";
 import Tasks from "./Tasks";
 
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
+
 export default function App() {
+  const months = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
+  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "saturday"]
+  const newdate = new Date();
+  const date = newdate.getDate();
+  const month = newdate.getMonth();
+  const day = newdate.getDay();
   return (
     <div>
       <div className="mainbox">
-        <div className="headerbox">
-          <p>Habitracker</p>
-          <p>Time</p>
-          <div className="settings">
-            <p>streak</p>
-            <p>settings</p>
+        <div className="header">
+          <div className="headerbox">
+            <h1>Habitracker</h1>
+            <p>{days[day] + " , " + months[month] +" "+ date }</p>
+            <div className="settings">
+              <p>🔥</p>
+              <p>⚙️</p>
+            </div>
           </div>
         </div>
         <div className="contentbox">
-          <div className="tasks">
+          <div className="tasksmainbox">
             <Tasks></Tasks>
           </div>
-          <div className="calendar">calendar</div>
+          <div className="rightbox">
+            <div className="calendar">
+              <FullCalendar
+                plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                initialView="dayGridMonth"
+                headerToolbar={{
+                  left: 'prev',
+                  center: 'title',
+                  right: 'next'
+                }}
+                height="50vh"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/Tasks.jsx
+++ src/Tasks.jsx
@@ -1,11 +1,77 @@
-import React from 'react'
-
+import React, { useRef } from 'react'
+import { useState } from 'react'
 export default function Tasks() {
+  const [tasks, settasks] = useState([])
+  const [donetasks, setdonetasks] = useState([])
+  const [txt, settxt] = useState("")
+
+  const id = useRef(1)
+
+  const addtask = (e) => {
+    e.preventDefault();
+    if (!txt) return alert("Enter a tasks to add to list")
+    settasks(tasks => [...tasks, { id: id.current++, text: txt, done: false }]);
+    settxt("")
+  }
+
+  const mark_as_done = (id, flag) => {
+    if (flag) {
+      const tasktomove = tasks.find(t => t.id === id)
+      if (!tasktomove) return prevtasks;
+      setdonetasks(prevDone => [...prevDone, { ...tasktomove, done: true }]);
+      settasks(prevtasks => prevtasks.filter(t => t.id !== id));
+    } else {
+      const tasktomove = donetasks.find(t => t.id === id);
+      if (!tasktomove) return taskDone;
+      settasks(prevtasks => [...prevtasks, { ...tasktomove, done: false }]);
+      setdonetasks(taskDone => taskDone.filter(t => t.id !== id));
+    }
+  }
+
+
+  const delete_task = (id, where) => {
+    if (where === 1) {
+      return settasks(removetask => removetask.filter(t => t.id != id));
+    } else {
+      return setdonetasks(removetask => (removetask.filter(t => t.id != id)));
+    }
+  }
+
   return (
-    <div>
-      <div>
-        
+    <>
+      <div className='tasksbox'>
+        <div className='listoftasks'>
+          <div className='tasks'>
+            <p id="header">Habits</p>
+            {tasks.map((t) => (
+              <div key={t.id} className='singletask'>
+                {t.text}
+                <input type="checkbox"
+                  checked={t.done}
+                  onChange={() => mark_as_done(t.id, true)} />
+                <button onClick={() => delete_task(t.id, 1)}>remove</button>
+              </div>
+            ))}
+          </div>
+          <div>
+            <p>Done</p>
+            {donetasks.map((t) => (
+              <div key={t.id} id={t.done ? "dash" : ""}>
+                {t.text}
+                <input type="checkbox"
+                  checked={t.done}
+                  onChange={() => mark_as_done(t.id, false)} />
+                <button onClick={() => delete_task(t.id, 0)}>remove</button>
+              </div>
+            ))}
+          </div>
+          <div className='inputbox'>
+            <form onSubmit={(e) => addtask(e)}>
+              <input type="text" name="" id="" value={txt} onChange={(e) => settxt(e.target.value)} />
+              <button type="submit">Add task</button></form>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
